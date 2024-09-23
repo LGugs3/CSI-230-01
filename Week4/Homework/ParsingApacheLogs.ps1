f4a937f05747076014adb336ed759b7fcb7c4ce5
Liam Gugliotta <liam.gugliotta@mymail.champlain.edu>
2024-09-22 23:01:45 -0400

    Completed Parsing Apache Logs Homework

---
+++ Week4/Homework/ParsingApacheLogs.ps1
@@ -0,0 +1,28 @@
+function ApacheLogs1()
+{
+    $unformattedLogs = Get-Content C:\xampp\apache\logs\access.log
+    $tableRecords = @()
+
+    #$unformattedLogs[-9]
+
+    for($i = 0; $i -lt $unformattedLogs.Count; $i++)
+    {
+        $words = $unformattedLogs[$i].Split(" ");
+        
+        $tableRecords += [pscustomobject]@{ "IP" = $words[0];
+                                            "Time" = $words[3].Trim('[');
+                                            "Method" = $words[5].Trim('"');
+                                            "Page" = $words[6];
+                                            "Protocol" = $words[7];
+                                            "Response" = $words[8];
+                                            "Referrer" = $words[10];
+                                            "Client" = $words[11..($words.Count - 1)];
+                                           }
+                                           
+    }
+    return $tableRecords | Where-Object {$_.IP -like "10.*" }
+}
+clear
+
+$tableLogs = ApacheLogs1
+$tableLogs | Format-Table -AutoSize -Wrap
