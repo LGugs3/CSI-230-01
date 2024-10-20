7301a5cc7197e644ed91d81a777aa221aabe92c8
Liam Gugliotta <liam.gugliotta@mymail.champlain.edu>
2024-10-20 19:54:52 -0400

    Started working on Midterm. some stuff needs debugging.

---
+++ Midterm/functions.ps1
@@ -0,0 +1,34 @@
+﻿function getPageByCom($url)
+{
+    $ie = New-Object -ComObject internetexplorer.application #Creating COM object
+
+    $ie.navigate($url)
+    while($ie.ReadyState -ne 4) {Start-Sleep -s 10} #Wait for page to load
+    $ie.visible = $false
+
+    return $ie
+}
+
+function getTable($page)
+{
+    $trs = $page.Document.getElementsByTagName("tr")
+    return $trs
+}
+
+function getRows($table)
+{
+    $headerRow = $table[0].getElementsByTagName("td")
+    $patternName = $headerRow[0].innerText
+    $descName = $headerRow[1].innerText
+    $formattedTable = @()
+    <#
+    for($i = 1; $i -le $table.length; $i++)
+    {
+        $row = $table[$i].getElementsByTagName("td")
+        $formattedTable += [pscustomobject] @{ $patternName = $row[0].innerText; `
+                                               $descName = $row[0].innerText; 
+                                             }
+    }
+    #>
+    return $formattedTable | Format-Table
+}
---
+++ Midterm/main.ps1
@@ -0,0 +1,12 @@
+﻿. (Join-Path $PSScriptRoot functions.ps1)
+clear
+
+$url = "http://10.0.17.15/IOC.html"
+
+$page = getPageByCom $url
+$table = getTable $page
+getRows $table
+
+
+
+$page.Quit()
