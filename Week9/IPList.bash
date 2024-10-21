b8f462ab09d28b6b9907a0e00664e7556156b4e7
Liam Gugliotta <liam.gugliotta@mymail.champlain.edu>
2024-10-21 12:53:09 -0400

    Completed Deliverable 1 && 2

---
+++ Week9/IPList.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Own IP is 10.0.17.21
+
+#Checks if prefix argument is provided
+[ $# -lt 1 ] && echo "Usage: $0 <Prefix>" && exit 1
+
+#Checks if the prefix is at least 5 characters long
+[ ${#1} -lt 5 ] && \
+printf "Prefix length too short\nPrefix Example: 10.0.17\n" && exit 1
+
+prefix=$1
+
+for i in {1..254}
+do
+	ping -c 1 $prefix.$i
+done
