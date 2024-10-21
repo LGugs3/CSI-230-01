e6e87744f3a59ddd95fecb4192fa0215eb78e035
Liam Gugliotta <liam.gugliotta@mymail.champlain.edu>
2024-10-20 21:00:32 -0400

    Did a few things. Arrays passed into filterLogsByPage() turns to null for some reason

--- Midterm/functions.ps1
+++ Midterm/functions.ps1
@@ -17,18 +17,50 @@ function getTable($page)
 
 function getRows($table)
 {
-    $headerRow = $table[0].getElementsByTagName("td")
-    $patternName = $headerRow[0].innerText
-    $descName = $headerRow[1].innerText
+    $headerRow = $table[0].getElementsByTagName("th")
+    $patternName = $headerRow[0].innerText.ToString()
+    $descName = $headerRow[1].innerText.ToString()
     $formattedTable = @()
-    <#
-    for($i = 1; $i -le $table.length; $i++)
+    
+    for($i = 1; $i -le $table.length - 1; $i++)
     {
         $row = $table[$i].getElementsByTagName("td")
         $formattedTable += [pscustomobject] @{ $patternName = $row[0].innerText; `
-                                               $descName = $row[0].innerText; 
+                                               $descName = $row[1].innerText; `
                                              }
     }
-    #>
+    
     return $formattedTable | Format-Table
+}
+
+function parseApacheLogs($filePath)
+{
+    $unformattedLogs = Get-Content $filePath
+    $table = @()
+
+    for($i = 0; $i -lt $unformattedLogs.Count; $i++)
+    {
+        $line = $unformattedLogs[$i].Split(" ")
+
+        $table += [pscustomobject]@{ "IP" = $line[0];
+                                     "Time" = $line[3].Trim('[');
+                                     "Method" = $line[5].Trim('"');
+                                     "Page" = $line[6];
+                                     "Protocol" = $line[7];
+                                     "Response" = $line[8];
+                                     "Referrer" = $line[10];
+                                   }
+    }
+    return $table | Format-Table
+}
+
+function filterLogsByPage($logTable, [pscustomobject]$comparisonTable)
+{
+    $comparisonTable | Out-String
+    $testTable = @()
+    for($i = 0; $i -lt $comparisonTable.Count; $i++)
+    {
+        $testTable += [pscustomobject] @{ "Description" = $comparisonTable[$i].Description; }
+    }
+    #return $logTable.Page | Select-String $testTable | Format-Table
 }
--- Midterm/main.ps1
+++ Midterm/main.ps1
@@ -2,11 +2,16 @@
 clear
 
 $url = "http://10.0.17.15/IOC.html"
+$logFilePath = "C:\Users\champuser\Desktop\CSI-230-01\Midterm\access.log"
 
 $page = getPageByCom $url
-$table = getTable $page
-getRows $table
+$comTable = getTable $page
+$formattedComTable = getRows $comTable
+$formattedComTable | Out-String
 
+$formattedApacheTable = parseApacheLogs $logFilePath
+$filteredApacheTable = filterLogsByPage($formattedApacheTable, $formattedComTable)
 
+$filteredApacheTable | Out-String
 
 $page.Quit()
