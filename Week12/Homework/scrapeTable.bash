e6a91fd7a0500eef6ad87d59bb95aa2e3b73c920
Liam Gugliotta <liam.gugliotta@mymail.champlain.edu>
2024-11-12 15:36:54 -0500

    Pretty sure competed week12 hw

--- Week12/Homework/scrapeTable.bash
+++ Week12/Homework/scrapeTable.bash
@@ -18,6 +18,7 @@ function scrapeColumn()
  #create file based on column name
  outputFile="${name}Table.txt"
  :> "$outputFile"
+ echo "$name" >> $outputFile
 
  #go row by row
  while [[ ! -z "$line" ]]
@@ -25,7 +26,9 @@ function scrapeColumn()
   line="$(cat "$fileName" | pup "table#$name tr:nth-of-type($counter) td:first-of-type text{}")"
   echo "$line" >> $outputFile
   counter=$(($counter + 1))
-done
+ done
+
+ echo "$outputFile"
 }
 
 function getDates()
@@ -34,19 +37,50 @@ function getDates()
  local line="start"
  local counter=2
  local name="temp"
+ local outputFile="dateTable.txt"
 
  #create file based on column name
- :> "dateTable.txt"
+ :> "$outputFile"
+ echo "date" >> "$outputFile"
 
  #row by row
  while [[ ! -z "$line" ]]
  do
-  line="$(cat "$fileName" | pup "table#$name tr:nth-of-type($counter) td:nth-of-type(2) text{}}")"
-  echo "$line" >> "dateTable.txt"
+  line="$(cat "$fileName" | pup "table#$name tr:nth-of-type($counter) td:last-of-type text{}")"
+  echo "$line" >> $outputFile
   counter=$(($counter + 1))
-done
+ done
+
+ echo "$outputFile"
 }
 
-scrapeColumn "temp"
-scrapeColumn "press"
-getDates
+function combineTables()
+{
+ local outputFile="combinedTable.txt"
+ local fileLength=$(wc -l $1 | cut -d' ' -f1) #assumes all files are the same length
+ local line=""
+ 
+ :> $outputFile
+
+ for i in $(seq 1 $fileLength)
+ do
+  line=""
+  for file in "$@"
+  do
+    line+="$(head -n $i $file | tail -1) "
+  done
+  echo "$line" >> $outputFile
+ done
+
+ echo "$outputFile"
+}
+
+colOne=$(scrapeColumn "press")
+colTwo=$(scrapeColumn "temp")
+colThree=$(getDates)
+
+echo "$colOne, $colTwo, $colThree"
+
+fileName=$(combineTables $colOne $colTwo $colThree)
+
+cat "$fileName"
